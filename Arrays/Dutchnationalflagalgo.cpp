ce1ade7b8b6cec4deaf728faad54c61668886d08
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-08-24 17:54:30 +0530

    Update Dutchnationalflagalgo.cpp

--- Arrays/Dutchnationalflagalgo.cpp
+++ Arrays/Dutchnationalflagalgo.cpp
@@ -1,1 +1,27 @@
 
+// // best way to solve sort 0's 1's and 2's arrays or similar questions , tc 0(n) and beats 97% of sol on leetcode in SC.
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int n = nums.size();
+       int count =0;
+       map<int, int>mp;
+       for(auto i:nums){
+          mp[i]++;
+       }
+
+       vector<int>ans;
+       for(int i =0;i<mp.size();i++){
+          int a = mp[i];
+          
+          for(int i =0;i<a;i++){
+            ans.push_back(count);
+          }
+          count++;
+       }
+
+       nums = ans;
+
+    }
+};
