f0e200f40f0507a9f71e6b4e57fbfe6e1246a2f4
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-10-21 21:06:53 +0530

    Add Coin Change problem solution in C++
    Implement coin change solution using dynamic programming..memoization approach , a hard one.

---
+++ Dynamic Programming /Coin change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public: //tc and sc will be varying as on a particular index there are multiple ways for recursion..
+
+//sc  O(n * amount) + O(amount) //tc  O(n * amount)
+int f (  int i , int amount , vector<int> & coins  , vector<vector<int>>& dp) {
+
+if ( i==0)
+{
+    if ( amount%coins[i]==0) return amount/coins[i];
+    else
+        return 1e9;
+}
+        if(dp[i][amount]!= -1) return dp[i][amount ];
+
+        int nottake = f(i-1, amount , coins , dp);
+        int take = INT_MAX ;
+        if ( coins[i]<=amount) 
+        take = 1+ f(i , amount-coins[i], coins , dp);
+
+        return dp[i][amount] = min( take , nottake);
+}
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+          vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+          int ans = f(n-1,amount , coins , dp);
+          if ( ans >=1e9)  return -1 ;
+          return ans ;
+    }
+};
