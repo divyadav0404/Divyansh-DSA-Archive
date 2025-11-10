e9d60578a11e5fd211fae4bfd5a22e9d44145e3c
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-11-10 19:03:26 +0530

    Create Find square root of a number.cpp
    Both methods are here..

---
+++ Binary search/BS ON ANSWERS /Find square root of a number.cpp
@@ -0,0 +1,32 @@
+// linear tc code
+class Solution {
+public:
+    int floorSqrt(int n) {
+        int ans = 1;
+        for (int i = 1; i  <= n; i++) {
+            if(i*i<=n)
+            {
+              ans = i ;
+        }
+        return ans;
+    }
+};
+ // log tc code 
+
+class Solution {
+public:
+    int floorSqrt(int n) {
+        int low = 1, high = n;
+        int ans = 1;
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+            if (mid * mid <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
