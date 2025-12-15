c92a9d7ee769f8373379dd6160e7dd12a4d6f7c0
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-12-16 03:52:09 +0530

    Update Max Consecutive Ones III.cpp( O(N) sol , sliding window one..

--- Sliding window and two ptr combined problems /Max Consecutive Ones III.cpp
+++ Sliding window and two ptr combined problems /Max Consecutive Ones III.cpp
@@ -27,3 +27,25 @@ public:
         return ans ;
     }
 };
+//time complexity O(N) where sliding window is used , code is here
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0 , zc = 0;
+        for ( int right = 0 ; right < nums.size();right++)
+        {
+            if(nums[right]==0)
+            zc++;
+
+        if ( zc > k)
+        {
+            if ( nums[left]==0)
+                zc--;
+            left ++;
+            
+        }
+        }
+        return nums.size()-left ;
+    }
+};
