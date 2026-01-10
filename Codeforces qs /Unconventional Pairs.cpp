bc191142c292dd4734572214a24dc52808d96889
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-01-10 17:51:49 +0530

    cf problem 2149B , sol is sorting and then loop and max of i+=2. of arr
    see for full sol,, my answer..

---
+++ Codeforces qs /Unconventional Pairs.cpp
@@ -0,0 +1,28 @@
+//2149B question
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        long long ans = 0;
+        for (int i = 0; i < n; i += 2) {
+            ans = max(ans, abs(a[i] - a[i + 1]));
+        }
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
