03e86e742c161d78b4981b5d83efa62ebb4ad135
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-01-12 00:06:00 +0530

    Add function to compare two string arrays for equality
    Implement function to check if two string arrays are equivalent by concatenating them.

--- Strings ( basic , med )/Check If Two String Arrays are Equivalent.cpp
+++ Strings ( basic , med )/Check If Two String Arrays are Equivalent.cpp
@@ -22,3 +22,16 @@ public:
             return false;
     }
 }; //concat and add
+// another easy code used stl here , one more sol is we can use stringstream or append fnc..
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+        string s1 = accumulate(word1.begin(), word1.end() , string(""));
+       string s2 =  accumulate(word2.begin(), word2.end(), string(""));
+        if ( s1==s2)
+        return true ;
+        else
+        return false ;
+    }
+};
