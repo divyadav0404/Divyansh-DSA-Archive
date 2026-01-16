25e369aba23cb2099c8653600b84febc961bedae
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-01-16 20:26:53 +0530

    Add solution for Codeforces problem 155A

---
+++ Codeforces qs /I_love_%username% 155A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int best = a[0];
+    int worst = a[0];
+    int count = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(a[i] > best) {
+            best = a[i];
+            count++;
+        }
+        else if(a[i] < worst) {
+            worst = a[i];
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
