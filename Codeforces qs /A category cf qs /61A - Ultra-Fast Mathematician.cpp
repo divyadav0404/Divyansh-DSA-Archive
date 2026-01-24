46d7b3757f3388629340b6c0bbdbfb09954d78ba
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-01-24 15:12:53 +0530

    basic use of xor is being depicted here.

---
+++ Codeforces qs /A category cf qs /61A - Ultra-Fast Mathematician.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   
+
+    string a, b;
+    cin >> a >> b;
+
+    int n = a.size();  
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == b[i])
+            cout << '0';
+        else
+            cout << '1';
+    }
+    cout << '\n';
+
+    return 0;
+}
