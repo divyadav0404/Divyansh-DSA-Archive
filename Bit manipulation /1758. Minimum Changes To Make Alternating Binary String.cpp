85a06adae0cf5faa70852318437781d6bccae1aa
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-03-05 18:32:46 +0530

    Optimize minOperations for alternating binary string
    Refactor minOperations function for better efficiency by using a more optimal approach based on observations.

--- Bit manipulation /1758. Minimum Changes To Make Alternating Binary String.cpp
+++ Bit manipulation /1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -32,3 +32,33 @@ public:
         return min ( sw_one , sw_zero ) ;
     }
 };
+// even more optimal approach
+class Solution {
+public:
+    int minOperations(string s) {
+         //a better appraoch than before , based on keen observation !!
+         int n = s.length ();
+         int swz = 0;
+         int sw1 = 0;
+         for ( int i = 0 ; i < n ; i ++)
+         {
+            if ( i % 2 == 0)
+            {
+                if ( s[i] == '1')
+                {
+                    swz  ++;
+                }
+            }
+                else
+                {
+                    if ( s[i]=='0')
+                    {
+                        swz ++;
+                    }
+
+                }
+            
+         }
+         return min ( swz , n - swz );
+    } 
+};
