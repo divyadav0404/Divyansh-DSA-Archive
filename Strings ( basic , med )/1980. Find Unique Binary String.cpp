6e0ad1cc4584d46cf33fccd230e3a80bdead4858
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-03-08 20:07:34 +0530

    cantor diagonal !!

---
+++ Strings ( basic , med )/1980. Find Unique Binary String.cpp
@@ -0,0 +1,19 @@
+class Solution { //cantor diagonalisation problem
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string ans = "" ;
+        for ( int i = 0 ; i < n ; i ++)
+        { 
+             if ( nums[i][i]=='0')
+             {
+                ans = ans + '1' ;
+             }
+             else
+             {
+                ans = ans + '0' ;
+             }
+        }
+        return ans ; //O(n) time complexity !!
+    }
+};
