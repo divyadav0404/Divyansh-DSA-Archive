22e4ad1fe3331bb722008e4c42ca5a4148fa1e91
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-03-09 03:44:51 +0530

    3 consecutive same integers !!!

---
+++ TCS NQT PYQ /spam.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+string s ;
+cin >> s ;
+for ( int i = 0 ; i < s.length()-2 ; i ++)
+{
+    if ( s[i]==s[i+1] && s[i]==s[i+2])
+    {
+        cout << "spam" << endl;
+        return 0;
+    }
+}
+cout << "not a spam" << endl;
+
+}
