a3c7aea394768b8a112e67bd4c9b63b682330001
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-03-12 04:52:36 +0530

    two good sol , one sort and one updating two  values using loop

---
+++ Arrays/1464. Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,33 @@
+//two sols
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+       sort (nums.begin(), nums.end());
+       return (nums[n-1] -1 ) * (nums[n-2]-1);
+
+    }
+};
+
+// second one
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0 ;
+        int sl = 0;
+        for ( int i = 0 ; i < n ; i ++)
+        {
+            if ( nums[i] > l)
+            {
+             sl = l ;
+             l = nums[i] ;
+            }
+            else
+            {
+                sl = max ( sl , nums[i]);
+            }
+        } 
+        return (l-1) * (sl - 1) ;
+    }
+};
