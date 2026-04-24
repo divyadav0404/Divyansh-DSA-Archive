336a1a305fa7277c5b0b87ae626ef15f174be46f
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-04-24 17:39:54 +0530

    compare ands sort

--- Arrays/2206. Divide Array Into Equal Pairs.cpp
+++ Arrays/2206. Divide Array Into Equal Pairs.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+     int n = nums.size();
+     sort(nums.begin(), nums.end());
+     for ( int i = 0 ; i < n ; i = i + 2)
+     {
+        if ( nums[i] != nums[i+1]) return false ;
+     }   
+     return true ;
+    }
+};
