005ab7ffe9463580f667d5cebe57c2aec35f01ee
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-05-07 18:38:51 +0530

    just simple sort and compare

---
+++ Telephonic Interviews LC Easy /1460. Make Two Arrays Equal by Reversing Subarrays.cpp
@@ -0,0 +1,16 @@
+// method 1 sort and compare
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        if (target == arr)
+        {
+            return true ;
+        }
+        return false ;
+    }
+};
+
+
+//
