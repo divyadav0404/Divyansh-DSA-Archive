3937417e1f158c74715a50c5e77e56e29093a138
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-06-06 19:08:17 +0530

    easy just do as said !!

---
+++ AtCoder Beginner Contest 461 /A - Armor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, d;
+    cin >> a >> d;
+
+    if (a <= d)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
