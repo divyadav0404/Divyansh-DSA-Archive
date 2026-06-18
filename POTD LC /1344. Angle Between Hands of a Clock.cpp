eaafa80521d99aa0df80f9d4786caad21523bc9e
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-06-18 20:16:36 +0530

    easy just think about angles related to hour and minute hands !!

---
+++ POTD LC /1344. Angle Between Hands of a Clock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double hourAngle = 30 * hour + 0.5 * minutes;
+        double minuteAngle = 6 * minutes;
+
+        double diff = abs(hourAngle - minuteAngle);
+
+        return min(diff, 360 - diff);
+    }
+};
