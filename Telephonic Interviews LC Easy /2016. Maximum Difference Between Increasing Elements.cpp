83be4dce768c622e23c0275a516989b62eff20bf
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-06-23 18:38:41 +0530

    brute force approach to solve this question !!

---
+++ Telephonic Interviews LC Easy /2016. Maximum Difference Between Increasing Elements.cpp
@@ -0,0 +1,19 @@
+ class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int diff = -1 ;
+        for( int i = 0 ; i < nums.size() ; i++)
+        {
+            for ( int j = i+1 ; j< nums.size(); j ++ )
+            {
+                if ( nums[j]  > nums[i])
+                {
+                    diff = max ( diff , abs(nums[j] - nums[i]));
+                
+                }
+            }
+        }
+        return diff;
+    }
+};
