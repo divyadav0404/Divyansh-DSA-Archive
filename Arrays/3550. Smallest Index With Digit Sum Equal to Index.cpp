a49090335024c99df84edfab88810ec3ae2b9614
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-09-24 17:56:54 +0530

    Add solution for smallest index with digit sum equal to index
    Implement a solution to find the smallest index where the sum of digits equals the index.

---
+++ Arrays/3550. Smallest Index With Digit Sum Equal to Index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+      int n = nums.size();
+      for ( int i = 0 ; i < n ; i ++)
+      {
+int x = nums[i];
+int s = 0 ;
+while ( x > 0){
+s += x % 10 ;
+x /= 10;
+      }  
+
+      if ( s == i)
+      return i ;
+    }
+
+    return -1 ;
+    }
+};
